dc10d0164da19e6038b1a56c67e6418a0deb9d91
Elliot Bruneel <elliot.bruneel@wizbii.com>
2021-12-02 23:00:51 +0100

    Day 01

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,34 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
+    fun getPreviousSum(input: List<String>, offset: Int): Int? {
+        if(offset < 0) return null
+        if(offset - 2 < 0) return null
+        return (offset -2..offset).map { input[it].toInt() }.sum()
     }
-
-    fun part2(input: List<String>): Int {
-        return input.size
-    }
-
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
 
     val input = readInput("Day01")
-    println(part1(input))
-    println(part2(input))
+
+    var totalIncrease = 0;
+    input.forEachIndexed { index, value ->
+        if (index > 0) {
+            val previous = input[index - 1]
+            if (value.toInt() > previous.toInt()) {
+                totalIncrease += 1
+            }
+        }
+    }
+    println("Total increase: $totalIncrease")
+
+    totalIncrease = 0
+    input.forEachIndexed { index, value ->
+        val previousSum = getPreviousSum(input, index -1)
+        val sum = getPreviousSum(input, index)
+
+        if(sum !== null && previousSum !== null) {
+            if (sum > previousSum) {
+                totalIncrease += 1
+            }
+        }
+    }
+
+    println("Total increase: $totalIncrease")
 }
